ce3f70d81498dc88fc453247f0063d31862f50d5
Maciej Pitucha <m.pitucha@camlintechnologies.com>
2016-07-15 16:11:10 +0200

    main function example

--- test/main.js
+++ test/main.js
@@ -1,10 +1,8 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-
-
 /**
  * using bundle.js
  */
+// import ReactDOM from 'react-dom'
+// import React from 'react'
 // import { main, FooComponent } from '../dist/bundle'
 
 // //main function usagel example
@@ -18,12 +16,21 @@ import React from 'react'
 
 
 /**
- * using bundle.js
+ * using lib: FooComponent
  */
+// import ReactDOM from 'react-dom'
+// import React from 'react'
+// import FooComponent from '../lib/fooComponent'
+// //react component usage example
+// ReactDOM.render(
+//   <FooComponent />,
+//   document.getElementsByTagName('body')[0]
+// );
 
-import FooComponent from '../lib/fooComponent'
-//react component usage example
-ReactDOM.render(
-  <FooComponent />,
-  document.getElementsByTagName('body')[0]
-);
+
+/**
+ * using lib: renderer
+ */
+ import renderer from '../lib/renderer'
+
+ renderer()
